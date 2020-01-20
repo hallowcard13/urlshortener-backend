354b65a0dd7fbcf90987f440b905faf2ca6be220
Hamid Reside <hamid.reside@gmail.com>
2020-01-20 22:43:19 +0800

    Done with getting links url

--- app/URLShortener.php
+++ app/URLShortener.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class URLShortener extends Model
 {
     //
+    public function getShortUrlAttribute($value)
+    {
+       return env('APP_URL').'/'.$value;
+    }
 }
