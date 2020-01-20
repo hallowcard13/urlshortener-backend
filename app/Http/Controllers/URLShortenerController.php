aa8aca837e7f6371a8db8b320860ac9b837551e7
Hamid Reside <hamid.reside@gmail.com>
2020-01-20 08:25:53 +0800

    Laravel installation and created a model, migration, and Controller for URL shortener

---
+++ app/Http/Controllers/URLShortenerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\URLShortener;
+use Illuminate\Http\Request;
+
+class URLShortenerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\URLShortener  $uRLShortener
+     * @return \Illuminate\Http\Response
+     */
+    public function show(URLShortener $uRLShortener)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\URLShortener  $uRLShortener
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(URLShortener $uRLShortener)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\URLShortener  $uRLShortener
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, URLShortener $uRLShortener)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\URLShortener  $uRLShortener
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(URLShortener $uRLShortener)
+    {
+        //
+    }
+}
---
+++ database/factories/URLShortenerFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\URLShortener;
+use Faker\Generator as Faker;
+
+$factory->define(URLShortener::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
